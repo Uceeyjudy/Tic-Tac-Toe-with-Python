4ee6353b1679efac23ab6b5fc97272e0c385db57
Judith Ezenwosu <ucjudith@gmail.com>
2022-03-11 15:55:53 +0100

    added the second function to validate user input

--- Tic Tac Toe with Python/main.py
+++ Tic Tac Toe with Python/main.py
@@ -8,3 +8,29 @@ def display(row1, row2, row3):
     print (row3)
 
 display(row1, row2, row3)
+position_index=int(input('Choose a position:'))
+
+def user_choice():
+    #VARIABLES
+    # initial values
+    choice='hello'
+    acceptable_values = range(0,10)
+    within_range = False
+    
+    # TWO CONDITIONS TO CHECK
+    # If choice is a digit or within the range
+    while choice.isdigit()== False or within_range==False:
+        choice= input('Enter a digit number (0-9): ')
+        
+        #DIGIT CHECK
+        if choice.isdigit()== False:
+            print('Sorry that is not a valid number')
+            
+        # RANGE CHECK
+        if choice.isdigit()==True:
+            if int(choice) in acceptable_values:
+                within_range= True
+            else:
+                print('Sorry that number is not within the range of 1-9')
+        
+    return int(choice)
